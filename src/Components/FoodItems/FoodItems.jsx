16f26d99f9f2070f311a737f67f55adf8c98f218
gergis-tse <gergistse@gmail.com>
2025-05-21 22:24:07 -0600

    Update FoodItems.jsx

--- src/Components/FoodItems/FoodItems.jsx
+++ src/Components/FoodItems/FoodItems.jsx
@@ -1,5 +1,5 @@
 import  { Component } from 'react'
-import  "./FoodItems.css"
+import  "./Fooditems.css"
 export default class FoodItems extends Component { 
   render() {
      console.log(this.props);
