d1e0033ad40e30f1195c8eda8877fbfb4eda4e86
Gergis <gergistse@gmail.com>
2025-05-21 22:54:53 -0600

    last commit

--- src/Components/FoodItems/FoodItems.jsx
+++ src/Components/FoodItems/FoodItems.jsx
@@ -1,5 +1,5 @@
 import  { Component } from 'react'
-import  "./FoodItems.CSS"
+import  "./Fooditems.css"
 export default class FoodItems extends Component { 
   render() {
      console.log(this.props);
